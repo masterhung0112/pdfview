62beb6244eecc2eaad26bc9d1444a01163d1f63c
hungbn <masterhung0112@gmail.com>
2021-06-12 14:58:49 +0700

    Add PdfDOcument class

---
+++ pdfsdk/src/main/java/com/hungknow/pdfsdk/PdfDocument.kt
@@ -0,0 +1,8 @@
+package com.hungknow.pdfsdk
+
+import android.os.ParcelFileDescriptor
+
+class PdfDocument(nativeDocPtr: Long, fileDescriptor: ParcelFileDescriptor) {
+//    private lateinit var mFileDescriptor: ParcelFileDescriptor
+//    private var mNativeDocPtr: Long = 0
+}
--- pdfsdk/src/main/java/com/hungknow/pdfsdk/PdfiumSDK.kt
+++ pdfsdk/src/main/java/com/hungknow/pdfsdk/PdfiumSDK.kt
@@ -1,8 +1,16 @@
 package com.hungknow.pdfsdk
 
+import android.os.ParcelFileDescriptor
+
 class PdfiumSDK {
     external fun nativeOpenDocument(fd: Int, password: String): Long
     external fun nativeOpenMemDocument(data: ByteArray, password: String): Long
+    private val mNativeDocPtr: Long = 0
+
+    fun newDocument(pfd: ParcelFileDescriptor, password: String): PdfDocument {
+        val nativeDocumentPtr = nativeOpenDocument(pfd.fd, password)
+        return PdfDocument(nativeDocumentPtr, pfd)
+    }
 
     companion object {
         init {
